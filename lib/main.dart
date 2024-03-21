e73f6c6e23a0742f06c6b1b3675d55dbe3b201bc
rahul-mohan-das <rl.4601@gmail.com>
2024-03-21 11:08:56 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class TVShowsPage extends StatefulWidget {
+  @override
+  _TVShowsPageState createState() => _TVShowsPageState();
+}
+
+class _TVShowsPageState extends State<TVShowsPage> {
+  @override
+  void initState() {
+    super.initState();
+    fetchShows();
+  }
+
+  List<dynamic> shows = [];
+  Future<void> fetchShows() async {
+    final response = await http.get(Uri.parse('https://api.tvmaze.com/shows'));
+    if (response.statusCode == 200) {
+      setState(() {
+        shows = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load shows');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TV Shows'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(shows.length, (index) {
+          return Card(
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    Image.network(
+                      shows[index]['image']['medium'] ??
+                          'https://via.placeholder.com/150',
+                      fit: BoxFit.cover,
+                      height: 100,
+                    ),
+                    Text(
+                      shows[index]['name'],
+                    ),
+                  ],
+                ),
+                //
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SecondRoute(
+                                //showId: index,
+                                )),
+                      );
+                    },
+                    child: Text("cast")),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class NewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Screen'),
+      ),
+      body: Center(
+          child: Column(
+              children:
+                  List.generate(_TVShowsPageState().shows.length, (index) {
+        final show = _TVShowsPageState().shows[index]["genres"][1];
+        return Text(show);
+      }))),
+    );
+  }
+}
+
+class SecondRoute extends StatefulWidget {
+  SecondRoute(/*{required this.showId}*/);
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  List<dynamic> castList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchShowCast();
+  }
+
+  Future<void> fetchShowCast() async {
+    final response =
+        await http.get(Uri.parse('https://api.tvmaze.com/shows/1/cast'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        castList = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load cast');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cast'),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: castList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(castList[index]['person']['name']),
+              subtitle: Text(castList[index]['character']['name']),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: TVShowsPage(),
+  ));
+}
